ed251c8051f045af1ed3db87cd8a96a48f60f400
sdpb <sdpalaciob@eafit.edu.co>
2020-08-12 21:36:43 -0500

    Improve code

--- Week5-8/index.php
+++ Week5-8/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
   </head>
   <body>
-    <div style="border: 5px dotted magenta;">
+    <div style="border: 5px solid violet;">
       <h1>Samuel Palacios Bernate PHP</h1>
       <pre>
           :####:
@@ -22,30 +22,14 @@
          .#####:
       </pre>
       <?php
-        print hash('sha256', 'Samuel Palacios Bernate');
+        $name = 'Samuel Palacios Bernate'
+        print "The SHA256 hash of \"$name\" is " . hash('sha256', $name);
       ?>
     </div>
-    <div style="border: 2px dotted red;">
-      <h2>Using another div with the list of etiquettes used in this file.</h2>
-      <p>This is just another test for <em>prettier</em> text.</p>
-      <ul>
-        <li>div</li>
-        <li>h1</li>
-        <li>p</li>
-        <li>ul</li>
-        <li>span</li>
-        <li>a</li>
-        <li>li</li>
-      </ul>
-    </div>
     <div>
-      <h3><strong>Where I found some info:</strong></h3>
-      <ul>
-        <li><a href="https://developer.mozilla.org">Mozilla developer</a></li>
-        <li><a href="https://devdocs.io/html/">Dev Docs</a></li>
-      </ul>
-      <p>IDK, that's all ;)... or not? <a href="http://www.uwu.com/">UwU</a></p>
+      <a href="fail.php">Fail</a>
+      <br />
+      <a href="check.php">Check</a>
     </div>
   </body>
 </html>
-
